daa08195a75bbe85d350ca57a6f7997ffc80691d
Boon Jhee <wbjhee@gmail.com>
2020-11-05 21:03:21 +0800

    added Data Models

---
+++ Models/StockData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace ItoCalculusProject.Models
+{
+    public class StockData
+    {
+        public int ID { get; set; }
+
+        public int OpenPrice { get; set; }
+        public int ClosePrice { get; set; }
+        public int Time { get; set; }
+    }
+}
