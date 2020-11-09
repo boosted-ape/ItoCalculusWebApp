cb36797a2d050c8955d7022c78daa4d02cc303d9
DESKTOP-7K259QB\Owner <wbjhee@gmail.com>
2020-11-09 20:13:51 +0800

    scaffolded identity

---
+++ ItoCalculusProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ItoCalculusProject.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ItoCalculusProject.Areas.Identity.IdentityHostingStartup))]
+namespace ItoCalculusProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
