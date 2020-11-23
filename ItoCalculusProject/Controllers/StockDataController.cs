ce5b0d4ac7750040706cbff678a3b30e08d8f457
Boon Jhee <wbjhee@gmail.com>
2020-11-23 20:10:15 +0800

    Added Controller

---
+++ ItoCalculusProject/Controllers/StockDataController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Net.Http;
+namespace ItoCalculusProject.Controllers
+{
+    public class StockDataController
+    {
+        public StockDataController()
+        {
+        }
+    }
+}
