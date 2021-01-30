2d814128bbff0509a99e94af15f04da5f1a6d39f
Boon Jhee <wbjhee@gmail.com>
2021-01-30 20:37:21 +0800

    edited react

--- pricemodels-react/src/App.js
+++ pricemodels-react/src/App.js
@@ -5,21 +5,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/stockData" className="navbar-brand">
+          bj
         </a>
-      </header>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/stockData"} className="nav-link">
+              Stock Data
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/","stockData"]} component={StockDataList}/>
+          <Route exact path ="/add" component={AddStockData}/>
+        </Switch>
+      </div>
     </div>
   );
 }
