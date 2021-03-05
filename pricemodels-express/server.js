165cde9df913e8968fe81990a3c13ef0418b9395
Boon Jhee <wbjhee@gmail.com>
2021-03-05 21:21:37 +0800

    added route

--- pricemodels-express/server.js
+++ pricemodels-express/server.js
@@ -24,6 +24,9 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
+
+require("./app/routes/stockData.routes")(app);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
